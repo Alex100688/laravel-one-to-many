033e1fb8acd807a7ea5cd6e40a4522d70687a162
Alex100688 <alessiozingaro2@gmail.com>
2023-02-08 03:58:43 +0100

    Creata una foreign key per projects

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Type;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 
 class TypeSeeder extends Seeder
 {
@@ -13,8 +15,16 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $types = ['frontend', 'backend', 'system', 'graphic_design','3D_artist'];
+
+        foreach($types as $type){
+            $new_type = new Type();
+            $new_type -> name = $type;
+            $new_type -> content = $faker->paragraph(2);
+            $new_type -> slug = Str::slug($new_type->name);
+            $new_type -> save();
+        }
     }
 }
