49543b24854014e099cc3fe90133efa4271df60f
Alex100688 <alessiozingaro2@gmail.com>
2023-02-08 04:10:44 +0100

    Creato un id type randomico per projects

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
 use App\Models\Project;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
@@ -18,11 +19,13 @@ class ProjectSeeder extends Seeder
     public function run(Faker $faker)
     {
         for($i = 0; $i < 10; $i++){
+            $type = Type::inRandomOrder()->first();
             $new_project = new Project();
             $new_project->title = $faker->sentence(4);
             $new_project->description = $faker->paragraph(2);
             $new_project->name_client = $faker->name(2);
             $new_project->slug = Str::slug($new_project -> title);
+            $new_project->type_id = $type -> id;
             $new_project->save();
         }
     }
